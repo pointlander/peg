bada9e753726db8fbf5561b68084af1ac579c097
Ivan Krasin <imkrasin@gmail.com>
2011-05-30 16:49:35 -0700

    Add build and test script. Dependency on roundup shell test framework is introduced

---
+++ env.sh
@@ -0,0 +1,20 @@
+if test -z "$GOROOT"
+then
+    # figure out what GOROOT is supposed to be
+    GOROOT=`printf 't:;@echo $(GOROOT)\n' | gomake -f -`
+    export GOROOT
+fi
+
+PKG_REQS="
+"
+#    goprotobuf.googlecode.com/hg/proto
+#    github.com/bmizerany/assert
+#    github.com/ha/doozer
+#"
+
+PKGS="
+    stl
+"
+
+CMDS="
+"
