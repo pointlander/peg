14e0cac4fd556844afa0caa972129253eb82bf58
Seth W. Klein <sk@sethwklein.net>
2014-08-04 08:00:34 -0400

    Add test that correctly spaced code parses

--- nospace_test.go
+++ nospace_test.go
@@ -2,6 +2,19 @@ package main
 
 import "testing"
 
+func TestCorrect(t *testing.T) {
+	buffer := `package p
+type T Peg {}
+Grammar <- !.
+`
+	p := &Peg{Tree: New(false, false), Buffer: buffer}
+	p.Init()
+	err := p.Parse()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestNoSpacePackage(t *testing.T) {
 	buffer := `packagenospace
 type T Peg {}
