fa2559758c439bef436422c0824db4289b453790
Andrew Snodgrass <git@pointlander.info>
2018-11-04 19:25:27 -0700

    Added basic java test

--- grammars/java/java_test.go
+++ grammars/java/java_test.go
@@ -15,6 +15,22 @@ import (
 	"testing"
 )
 
+var example1 = `public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+}
+`
+
+func TestBasic(t *testing.T) {
+	java := &Java{Buffer: example1}
+	java.Init()
+
+	if err := java.Parse(); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestJava(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping java parsing long test")
