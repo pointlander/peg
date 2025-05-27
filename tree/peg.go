ad9690f3362652cb47c20b61f613ae95fe28f7b1
Marcel Meyer <mm.marcelmeyer@gmail.com>
2025-05-28 00:51:19 +0200

    Add template parse error handling

--- tree/peg.go
+++ tree/peg.go
@@ -1191,7 +1191,12 @@ func (t *Tree) Compile(file string, args []string, out io.Writer) (err error) {
 		t.PegRuleType = "uint16"
 	}
 
-	err = template.Must(template.New("peg").Parse(pegHeaderTemplate)).Execute(&buffer, t)
+	tmpl, err := template.New("peg").Parse(pegHeaderTemplate)
+	if err != nil {
+		return err
+	}
+
+	err = tmpl.Execute(&buffer, t)
 	if err != nil {
 		return err
 	}
