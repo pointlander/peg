051eba3b4e0f323ab61cb1a6214892ffda50fe4a
Olivier Mengué <dolmen@cpan.org>
2026-05-18 12:15:37 +0200

    Use "go tool peg" in "//go:generate" lines in grammars
    Now that that 'peg' itself is registered as a tool in go.mod we can call
    it directly via 'go tool peg' in '//go:generate' lines (in grammars):
    peg will be automatically built (and cached) as necessary.

--- grammars/calculatorast/calculator_test.go
+++ grammars/calculatorast/calculator_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:generate ../../peg -switch -inline calculator.peg
+//go:generate go tool peg -switch -inline calculator.peg
 
 package calculatorast
 
--- grammars/java/java_test.go
+++ grammars/java/java_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:generate ../../peg -switch -inline java_1_7.peg
+//go:generate go tool peg -switch -inline java_1_7.peg
 
 package java
 
