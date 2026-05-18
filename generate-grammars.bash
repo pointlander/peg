b2d54e227f98f2fe42dc57ee1aec50d7882319a4
Olivier Mengué <dolmen@cpan.org>
2026-05-18 12:28:26 +0200

    generate-grammars.bash: simplify
    * Simplify generate-grammars.bash using Go package patterns.
    * Forward flags to 'go generate'
    
    Example:
    $ ./generate-grammars.bash -n
    go tool peg -switch -inline c.peg
    go tool peg -switch -inline calculator.peg
    go tool peg -switch -inline calculator.peg
    go tool peg -switch -inline fexl.peg
    go tool peg -switch -inline java_1_7.peg
    go tool peg -switch -inline long.peg

--- generate-grammars.bash
+++ generate-grammars.bash
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-set -Eeuo pipefail
-
-(cd grammars/c/ && go generate)
-(cd grammars/calculator/ && go generate)
-(cd grammars/calculatorast/ && go generate)
-(cd grammars/fexl/ && go generate)
-(cd grammars/java/ && go generate)
-(cd grammars/longtest/ && go generate)
+exec go generate "$@" ./grammars/...
