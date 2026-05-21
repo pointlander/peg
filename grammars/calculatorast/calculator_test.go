bae02155f78c267d9cfa242eccba18aa98412916
Olivier Mengué <dolmen@cpan.org>
2026-05-21 23:29:44 +0200

    grammars: generate in strict mode (peg -strict) (#199)
    Change the '//go:generate' in grammars/... to use -strict mode.
    
    $ go generate -x ./grammars/...
    go tool peg -strict -switch -inline c.peg
    go tool peg -strict -switch -inline calculator.peg
    go tool peg -strict -switch -inline calculator.peg
    go tool peg -strict -switch -inline fexl.peg
    go tool peg -strict -switch -inline java_1_7.peg
    go tool peg -strict -switch -inline long.peg

--- grammars/calculatorast/calculator_test.go
+++ grammars/calculatorast/calculator_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:generate go tool peg -switch -inline calculator.peg
+//go:generate go tool peg -strict -switch -inline calculator.peg
 
 package calculatorast
 
